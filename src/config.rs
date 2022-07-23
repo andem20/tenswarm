ba50771229593e11f37d6c463031042ca6ac1fbb
andem20 <andem20@student.sdu.dk>
2022-07-23 11:49:36 +0200

    refactor config

--- src/config.rs
+++ src/config.rs
@@ -1,3 +1,3 @@
-pub const NUM_REQUESTS: usize = 100;
-pub const URL: &'static str = "localhost:9090";
 pub const NUM_CLIENTS: usize = 100;
+pub const NUM_REQUESTS: usize = 100;
+pub const URL: &'static str = "localhost:10000";
